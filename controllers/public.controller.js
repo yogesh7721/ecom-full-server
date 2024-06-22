ccff8e2087b85a766255dd57413b096943dffe78
yogesh <shelkeyogesh874@gmail.com>
2024-06-22 15:07:38 +0530

    controller update

--- controllers/public.controller.js
+++ controllers/public.controller.js
@@ -1,9 +1,14 @@
 //publicGetALLproducts
 //publicGetProductDetails
+const asyncHandler = require("express-async-handler")
+const Product = require("../model/Product")
 
 exports.publicGetAllProduct = asyncHandler(async (req, res) => {
-    res.json({ message: "public product Success" })
+    const result = await Product.find({ active: true })
+
+    res.json({ message: "public product Success", result })
 })
 exports.publicGetProductDetails = asyncHandler(async (req, res) => {
-    res.json({ message: "public Product Details Success" })
+    const result = await Product.findById(req.param.id)
+    res.json({ message: "public Product Details Success", result })
 })
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,19 +3,25 @@
 // userUpdatePassword
 // userPlaceOrder
 // userCancelOrder
+const asyncHandler = require("express-async-handler")
+
 
 exports.userGetAllOrders = asyncHandler(async (req, res) => {
-    res.json({ message: "User Order Success" })
+    const result = await Order.find({ user: req.params.id })
+    res.json({ message: "User Order Success", result })
 })
 exports.userGetOrderDetails = asyncHandler(async (req, res) => {
-    res.json({ message: "user Order Details Success" })
+    const result = await Order.findById(req.params.id)
+    res.json({ message: "user Order Details Success", result })
 })
 exports.userUpdatePassword = asyncHandler(async (req, res) => {
     res.json({ message: "user update Password Success" })
 })
 exports.userPlaceOrder = asyncHandler(async (req, res) => {
+    await Order.create(req.order)
     res.json({ message: "user place order Success" })
 })
 exports.userCancelOrder = asyncHandler(async (req, res) => {
+    await Order.findByIdAndUpdate(req.params.id, { status: "cancel" })
     res.json({ message: "user CancelOrder Success" })
 })
